9f0029da51064dc76a68673b9f116579066cb4b2
Everton Luiz Bianchi Siqueira <evertonbianchi5@gmail.com>
2026-06-21 14:26:59 -0300

    renomeado a classe 'Marca' para 'Marcas' no modelo de dados

--- marcas.py
+++ marcas.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel, Field
 
-class Marca(BaseModel):
+class Marcas(BaseModel):
     id: int | None = None
     nome_marca: str = Field(min_length=2, max_length=100)
     pais_origem: str = Field(min_length=2, max_length=100)
