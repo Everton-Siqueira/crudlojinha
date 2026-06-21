e592a25c361260dd395ae3a2624d7bff1a2dbe95
Everton Luiz Bianchi Siqueira <evertonbianchi5@gmail.com>
2026-06-21 17:58:12 -0300

    adicionado o arquivo main.py com configuração inicial do FastAPI e inclusão de rotas

---
+++ crudlojinha/main.py
@@ -0,0 +1,29 @@
+import uvicorn
+from fastapi import FastAPI
+
+
+from controladores.controlador_clientes import router as clientes_router
+from controladores.controlador_produto import router as produto_router
+from controladores.controlador_marcas import router as marcas_router
+from controladores.controlador_pedidos import router as pedidos_router
+from controladores.controlador_itens_compras import router as itens_compras
+
+
+app = FastAPI()
+
+
+app.include_router(clientes_router)
+app.include_router(produto_router)
+app.include_router(marcas_router)
+app.include_router(pedidos_router)
+app.include_router(itens_compras)
+
+
+
+
+if __name__ == '__main__':
+    uvicorn.run(
+        'main:app',
+        port = 80,
+        reload = True
+    )
