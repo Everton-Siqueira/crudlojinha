8be0f177e71d6e479e8a045f9881ba90b65f9050
Everton Luiz Bianchi Siqueira <evertonbianchi5@gmail.com>
2026-06-28 13:29:27 -0300

    refatorar: substituir a URL do banco de dados por uma variável de ambiente

--- crudlojinha/banco_dados.py
+++ crudlojinha/banco_dados.py
@@ -1,2 +1,3 @@
-# banco_dados.py
-DATABASE_URL = "postgresql://YwMgiALSK8ksLyWc8fq6XF9qQGArHurh@dpg-d8uiffbeo5us73dvqdi0-a.oregon-postgres.render.com/banco_c99m?sslmode=require"
+import os
+
+DATABASE_URL = os.getenv("DATABASE_URL")
