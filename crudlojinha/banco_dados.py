ca70234a2b28fdcbc72187a628283e4723e1c172
Everton Luiz Bianchi Siqueira <evertonbianchi5@gmail.com>
2026-06-28 12:22:11 -0300

    remover lógica de configuração da DATABASE_URL e definir URL diretamente

--- crudlojinha/banco_dados.py
+++ crudlojinha/banco_dados.py
@@ -1,16 +1,2 @@
-import os
-
-# 1. Tenta ler o que está no painel do Render
-DATABASE_URL = os.getenv("DATABASE_URL")
-
-# 2. Se o Render falhar em passar a variável, usamos a URL completa diretamente aqui
-if not DATABASE_URL:
-    DATABASE_URL = "postgresql://YwMgiALSK8ksLyWc8fq6XF9qQGArHurh@dpg-d8uiffbeo5us73dvqdi0-a.oregon-postgres.render.com/banco_c99m?sslmode=require"
-else:
-    # Correção caso o Render envie como 'postgres://'
-    if DATABASE_URL.startswith("postgres://"):
-        DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
-        
-    # Garante o parâmetro de SSL ativo
-    if "?sslmode=" not in DATABASE_URL:
-        DATABASE_URL += "?sslmode=require"
+# banco_dados.py
+DATABASE_URL = "postgresql://YwMgiALSK8ksLyWc8fq6XF9qQGArHurh@dpg-d8uiffbeo5us73dvqdi0-a.oregon-postgres.render.com/banco_c99m?sslmode=require"
