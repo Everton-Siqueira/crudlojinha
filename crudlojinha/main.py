472d793fb113af2f71078fd943b4ccf8d323645f
Everton Luiz Bianchi Siqueira <evertonbianchi5@gmail.com>
2026-06-28 13:28:44 -0300

    refatorar: melhorar a formatação do retorno da raiz e remover comentários desnecessários

--- crudlojinha/main.py
+++ crudlojinha/main.py
@@ -1,6 +1,6 @@
+import os
 import uvicorn
 from fastapi import FastAPI
-
 
 from controladores.controlador_clientes import router as clientes_router
 from controladores.controlador_produto import router as produto_router
@@ -18,7 +18,10 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Bem-vindo à API da CrudLojinha!", "status": "ok"}
+    return {
+        "message": "Bem-vindo à API da CrudLojinha!",
+        "status": "ok"
+    }
 
 app.include_router(clientes_router)
 app.include_router(produto_router)
@@ -26,21 +29,12 @@ app.include_router(marcas_router)
 app.include_router(pedidos_router)
 app.include_router(itens_compras)
 
-
-
 Clientes.model_rebuild()
 Produto.model_rebuild()
 Marcas.model_rebuild()
 Pedidos.model_rebuild()
 Itens_Compras.model_rebuild()
 
-if __name__ == '__main__':
-    # Lê a porta que o Render vai te dar (padrão 10000 no Render se não achar)
-    port = int(os.getenv("PORT", 8000)) 
-    
-    uvicorn.run(
-        'main:app',
-        host='0.0.0.0', # Importante: mude para 0.0.0.0 para aceitar conexões externas
-        port=port,
-        reload=False # Desative o reload em produção no Render
-    )
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
