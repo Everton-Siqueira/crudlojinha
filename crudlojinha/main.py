0ca8f899fd14063b4ddba1a856c30906cc8a55d7
Everton Luiz Bianchi Siqueira <evertonbianchi5@gmail.com>
2026-06-29 20:42:05 -0300

    refatorar: comentar linha que força a exclusão das tabelas antigas no main.py

--- crudlojinha/main.py
+++ crudlojinha/main.py
@@ -24,7 +24,7 @@ app = FastAPI()
 Base.metadata.create_all(bind=engine)
 
 # 3. Esta linha força a exclusão das tabelas antigas e desatualizadas
-Base.metadata.drop_all(bind=engine)
+#Base.metadata.drop_all(bind=engine)
 
 # Esta linha cria todas as tabelas novas com as colunas corretas
 Base.metadata.create_all(bind=engine)
